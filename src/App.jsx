209cc79f55434ccad1d835a1f32e7c35a8cbd77d
veereshkumar <veereshkumar.b.s18@gmail.com>
2025-07-30 16:47:56 +0530

    sixth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import About from '../src/components/About';
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import ProgramsSection from './components/ProgramsSection';
+import ContactInfo from './components/ContactInfo';
+import Footer from './components/Footer';
+import './styles/About.css';
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <About />
-    </BrowserRouter>
+    <div>
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <ProgramsSection />
+      <ContactInfo />
+      <Footer />
+    </div>
   );
-}
+};
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,67 +1,9 @@
 import React from 'react';
-import '../styles/Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-container">
-        <div className="footer-column">
-          <h4>Uptoskills</h4>
-          <ul>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Careers</a></li>
-            <li><a href="#">Press</a></li>
-          </ul>
-        </div>
-
-        <div className="footer-column">
-          <h4>Programs</h4>
-          <ul>
-            <li><a href="#">Internships</a></li>
-            <li><a href="#">Open Source</a></li>
-            <li><a href="#">Hackathons</a></li>
-          </ul>
-        </div>
-
-        <div className="footer-column">
-          <h4>Resources</h4>
-          <ul>
-            <li><a href="#">Help Center</a></li>
-            <li><a href="#">Community</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </div>
-
-        <div className="footer-column">
-          <h4>Get in Touch</h4>
-          <div className="social-icons">
-            <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" /></a>
-            <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Twitter" /></a>
-            <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733614.png" alt="LinkedIn" /></a>
-            <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733585.png" alt="Instagram" /></a>
-          </div>
-
-          <div className="newsletter">
-            <h4>Newsletter</h4>
-            <div className="newsletter-form">
-              <input type="email" placeholder="Enter your email" />
-              <button>Subscribe</button>
-            </div>
-          </div>
-
-          <div className="contact-form">
-            <h4>Contact Us</h4>
-            <input type="text" placeholder="Your Name" />
-            <input type="email" placeholder="Your Email" />
-            <button>Send Message</button>
-          </div>
-        </div>
-      </div>
-
-      <div className="footer-bottom">
-        © 2025 Uptoskills. All rights reserved.
-      </div>
+      © 2025 UpToSkills. All rights reserved.
     </footer>
   );
 };
